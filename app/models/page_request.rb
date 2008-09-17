70d03c2e0b5b233de3612bfca29e9807f09dac7f
Jim Gay <jim@saturnflyer.com>
2008-09-17 15:30:52 -0400

    display page_request info, add popularity methods to PageReqest

--- app/models/page_request.rb
+++ app/models/page_request.rb
@@ -7,4 +7,14 @@ class PageRequest < ActiveRecord::Base
     check_url = check_url[1..-1] if check_url.match(/^\/\w+/)
     self.find(:first, :conditions => {:url => check_url})
   end
+  
+  def self.find_popular(num=25)
+    page_requests = PageRequest.find(:all, :order => 'count_created DESC', :limit => num)
+  end
+  
+  def popularity
+    count_max = PageRequest.maximum('count_created')
+    popularity = count_created/count_max.to_f
+    result = sprintf("%.0f", popularity * 100)
+  end
 end
