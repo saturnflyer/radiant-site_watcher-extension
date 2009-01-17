27e2fd1b80a3ea9c6c470cf88595267657015dfe
Jim Gay <jim@saturnflyer.com>
2009-01-16 23:54:49 -0500

    add ability to clear all requests

--- spec/controllers/page_requests_controller_spec.rb
+++ spec/controllers/page_requests_controller_spec.rb
@@ -15,7 +15,7 @@ describe Admin::PageRequestsController do
       lambda {PageRequest.find(@id)}.should raise_error(ActiveRecord::RecordNotFound)
     end
     it "should redirect back to the originating page" do
-      response.should redirect_to(:back)
+      response.should redirect_to(dashboard_path)
     end
     it "should display the message 'The page request mercury has been deleted. It had been requested 1 times.'" do
       flash[:message].should == 'The page request mercury has been deleted. It had been requested 1 times.'
@@ -33,7 +33,24 @@ describe Admin::PageRequestsController do
       page_requests(:first).ignore.should be_true
     end
     it "should redirect back to the originating page" do
-      response.should redirect_to(:back)
+      response.should redirect_to(dashboard_path)
+    end
+  end
+  
+  describe "when receiving a PUT request to /admin/page_requests/destroy_all" do
+    before(:each) do
+      login_as :admin
+      request.env["HTTP_REFERER"] = "http://test.host/admin/dashboard"
+      put :destroy_all
+    end
+    it "should delete all page_requests from the database" do
+      PageRequest.find(:all).size.should == 0
+    end
+    it "should redirect back to the originating page" do
+      response.should redirect_to(dashboard_path)
+    end
+    it "should display the message 'All page request records have been deleted. You've got a cleane slate!'" do
+      flash[:message].should == "All page request records have been deleted. You've got a cleane slate!"
     end
   end
 end
