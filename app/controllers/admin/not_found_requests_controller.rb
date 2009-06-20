d13699e3639288bf517bf163b2c45129bf21b632
Jim Gay <jim@saturnflyer.com>
2009-06-20 07:20:31 -0400

    fix destroy errors

--- app/controllers/admin/not_found_requests_controller.rb
+++ app/controllers/admin/not_found_requests_controller.rb
@@ -1,8 +1,8 @@
 class Admin::NotFoundRequestsController < ApplicationController
   def destroy
     @not_found_request = NotFoundRequest.find(params[:id])
-    @page_request_url = @page_request.url
-    @page_request_count = @page_request.count_created
+    @page_request_url = @not_found_request.url
+    @page_request_count = @not_found_request.count_created
     if @not_found_request.destroy
       flash[:message] = "The page request #{@page_request_url} has been deleted. It had been requested #{@page_request_count} times."
       redirect_to dashboard_path
